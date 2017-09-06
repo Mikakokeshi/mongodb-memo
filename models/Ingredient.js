0b54ba51b569637b7f16efbf84d64421543b1016
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-09-06 19:44:12 +0800

    initial commit

---
+++ models/Ingredient.js
@@ -0,0 +1,19 @@
+const mongoose =  require ( mongoose );
+const ( Schema ) = mongoose;
+
+mongoose.connect('mongodb://localhost/recipes');
+const { connection: db } = mongoose;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log (' connected to recipe database')
+});
+
+const ingredientSchema = new Schema({
+  name: String,
+  created_at: {type: Date, default: Date.now}
+})
+
+
+const Ingredient = mongoose.model('Ingredient', ingredientSchema);
+module.exports = Ingredient;
